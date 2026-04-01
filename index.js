9ecb42e960e3f98e1570e7f7ea592cb5d76d66c3
danishallani2026-hub <danishallani2020@gmail.com>
2026-04-01 14:45:15 +0800

    Add passenger counter files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let countEl = document.getElementById("count-el"); // pass in arguments
+let count = 0;
+
+let saveEl = document.getElementById("save-el");
+
+function increment() {
+    count++;
+    countEl.textContent = count;
+}
+
+function save() {
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    
+    count = 0;
+    countEl.textContent = 0;
+}
